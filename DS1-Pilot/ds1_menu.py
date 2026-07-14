6e1081c48a85791c1aea408cbaae45e5e50f8c29
dkrasicky-ux <dkrasicky@gmail.com>
2026-07-14 17:34:35 -0400

    Add menu items and get_menu function

---
+++ DS1-Pilot/ds1_menu.py
@@ -0,0 +1,8 @@
+menu_items = [
+    {"name": "Brisket Sandwich", "station": "Cutting", "prep_time": 10},
+    {"name": "Pulled Pork Bowl", "station": "Assembly", "prep_time": 8},
+    {"name": "Smoked Wings", "station": "Sides", "prep_time": 12}
+]
+
+def get_menu():
+    return menu_items
